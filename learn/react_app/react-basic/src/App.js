2d006533395a192e07ed4a877a9ea68b1479f96b
Sumio K <tap.miwo@gmail.com>
2022-10-09 09:51:52 +0900

    useEffect

--- learn/react_app/react-basic/src/App.js
+++ learn/react_app/react-basic/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import ToggleButton from './components/ToggleButton';
 import Counter from './components/counter';
 import TextInput from './components/TextInput';
+import CounterEffect from './components/counterEffect';
 
 function App() {
   return (
     <div className="App">
-      <TextInput />
+      {/* <TextInput /> */}
       {/* <ToggleButton /> */}
       {/* <Counter /> */}
+      <CounterEffect />
     </div>
   );
 }
---
+++ learn/react_app/react-basic/src/components/counterEffect.jsx
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from 'react'
+
+const CounterEffect = () => {
+    const [count, setCount] = useState(0)
+    const countUp = () => {
+        setCount(prevState => prevState + 1)
+    }
+    const countDown = () => {
+        setCount(prevState => prevState - 1)
+    }
+    
+    useEffect(() => {
+        console.log("Current count is...", count)
+    })
+    return(
+        <div>
+            <p>現在のカウント数: {count} </p>
+            <button onClick={countUp}>up</button>
+            <button onClick={countDown}>down</button>
+        </div>
+    );
+};
+export default CounterEffect;
